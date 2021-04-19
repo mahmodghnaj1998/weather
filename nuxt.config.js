18a3e0139aa1d4284e1f6260dc2a2cb6af72091e
mahmodghnaj <mahmodghnaj1998@gmail.com>
2021-04-20 02:23:41 +0500

    first

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,7 +39,10 @@ export default {
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
-  axios: {},
+  axios: {
+    proxyHeaders: false,
+    credentials: false
+  },
 
   // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
   vuetify: {
